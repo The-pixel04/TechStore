ae4e1d141fa2cfad6c75ca08715a0466c27ca9d5
The-pixel04 <krsyrdyrd@gmail.com>
2025-02-05 21:24:37 +0200

    Middleware

--- src/index.js
+++ src/index.js
@@ -15,7 +15,13 @@ try {
     console.error('Error connecting to MongoDB: ', err);
 }
 
-app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.engine('hbs', handlebars.engine({ 
+    extname: '.hbs' ,
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true,
+    }
+}));
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
 
